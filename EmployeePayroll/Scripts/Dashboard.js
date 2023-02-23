5b94d00d50636dd66366a5b90dfecda12326906b
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-23 23:48:55 +0530

    [Karan] addind the dasboard.js file

--- EmployeePayroll/Scripts/Dashboard.js
+++ EmployeePayroll/Scripts/Dashboard.js
@@ -1,50 +1,33 @@
-$(document).ready(function () {
-    // Make a GET request to the specified URL
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/employees",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (response) {
-            // Clear the table body
-            $("tbody").empty();
+$.ajax({
+    url: 'http://localhost:3000/employee',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+        var tableBody = $('#display tbody');
 
-            // Loop through each employee and add a row to the table for them
-            for (var i = 0; i < response.length; i++) {
-                // Create a new table row
-                var row = $("<tr>");
+        for (var i = 0; i < response.length; i++) {
+            // Create a new table row
+            var row = $("<tr>");
 
-                // Add a cell for each property of the employee
-                $("<td>").text(response[i].name).appendTo(row);
-                $("<td>").text(response[i].profileUrl).appendTo(row);
-                $("<td>").text(response[i].gender).appendTo(row);
-                $("<td>").text(response[i].departMent).appendTo(row);
-                $("<td>").text(response[i].salary).appendTo(row);
-                $("<td>").text(response[i].day).appendTo(row);
-                $("<td>").text(response[i].month).appendTo(row);
-                $("<td>").text(response[i].year).appendTo(row);
-               
+        // $.each(data, function (index, employee) {
+        //     var row = $('<tr>');
+            row.append($('<td>').text(response[i].profileUrl));
+            row.append($('<td>').text(response[i].name));  
+            row.append($('<td>').text(response[i].gender));
+            row.append($('<td>').text(response[i].departMent));
+            row.append($('<td>').text(response[i].salary));
+            row.append($('<td>').text(response[i].startDate));
+            var actions = $('<td>');
+            actions.append($('<button>').text('Edit').click(function () {
+            
+            }));
+            actions.append($('<button>').text('Delete').click(function () {
+            }));
+            row.append(actions);
 
-                // Add a cell for the delete button
-                var deleteButtonCell = $("<td>");
-                var deleteButton = $("<button>").addClass("fa fa-trash");
-                deleteButton.click(function () {
-                    // Code to delete the employee goes here
-                });
-                deleteButton.appendTo(deleteButtonCell);
-
-                // Add the row to the table
-                row.append(deleteButtonCell);
-                $("tbody").append(row);
-            }
-        },
-        failure: function (response) {
-            alert(response.responseText);
-            alert("Failure");
-        },
-        error: function (response) {
-            alert(response);
-            alert("Error");
-        }
-    });
+            tableBody.append(row);
+        });
+    },
+    error: function (xhr, textStatus, errorThrown) {
+    }
 });
