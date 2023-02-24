29aff535bf8b493deeaee226a8079caf857cbcc1
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-24 18:10:54 +0530

    [Karan] DELETE  method use for delete  data

--- EmployeePayroll/Scripts/Dashboard.js
+++ EmployeePayroll/Scripts/Dashboard.js
@@ -8,7 +8,7 @@ $.ajax({
         $.each(data, function (index, employee) {
             var row = $('<tr>');
             row.append($('<td>').html('<img src="' + employee.profileUrl + '">'));
-            row.append($('<td>').text(employee.name));  
+            row.append($('<td>').text(employee.name));
             row.append($('<td>').text(employee.gender));
             row.append($('<td>').text(employee.departMent));
             row.append($('<td>').text(employee.salary));
@@ -16,8 +16,12 @@ $.ajax({
             var actions = $('<td>');
             actions.append($('<img>').attr('src', '../Assets/icons/create-black-18dp.svg').click(function () {
             }));
-            actions.append($('<img>').attr('src', '../Assets/icons/delete-black-18dp.svg').click(function () {
+
+            actions.append($('<img>').attr('src', '../Assets/icons/delete-black-18dp.svg').attr('data-id', employee.id).click(function () {
+                var employeeId = $(this).attr('data-id');
+                deleteEmployee(employeeId);
             }));
+
             row.append(actions);
 
             tableBody.append(row);
@@ -26,3 +30,25 @@ $.ajax({
     error: function (xhr, textStatus, errorThrown) {
     }
 });
+
+function deleteEmployee(employeeId) {
+    $.ajax({
+        url: 'http://localhost:3000/employee/' + employeeId,
+        type: 'DELETE',
+        success: function () {
+            $('tr[data-id="' + employeeId + '"]').remove();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error deleting employee: ' + errorThrown);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
