50afac14ca3775ba49cf4c2b1b7eb04f6fa3f6c7
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-27 22:29:47 +0530

    [Karan] updating the data

--- EmployeePayroll/Scripts/Dashboard.js
+++ EmployeePayroll/Scripts/Dashboard.js
@@ -7,15 +7,59 @@ $.ajax({
 
         $.each(data, function (index, employee) {
             var row = $('<tr>');
-            row.append($('<td>').html('<img src="' + employee.profileUrl + '">'));
-            row.append($('<td>').text(employee.name));
+            row.append($('<td>').append(
+                $('<img>').attr('src', employee.profileUrl),
+                $('<span>').text(employee.name)
+            ));
             row.append($('<td>').text(employee.gender));
             row.append($('<td>').text(employee.departMent));
             row.append($('<td>').text(employee.salary));
             row.append($('<td>').text(employee.startDate));
             var actions = $('<td>');
+            // actions.append($('<img>').attr('src', '../Assets/icons/create-black-18dp.svg').click(function () {
+
+            // }));
             actions.append($('<img>').attr('src', '../Assets/icons/create-black-18dp.svg').click(function () {
+                var employeeId = $(this).attr('data-id');
+                var currentRow = $('tr[data-id="' + employeeId + '"]');
+
+                // Prompt the user to enter new data for the employee
+                var newName = prompt('Enter new name:');
+
+                var newGender = prompt('Enter new gender:');
+                var newDepartment = prompt('Enter new department:');
+                var newSalary = prompt('Enter new salary:');
+                var newStartDate = prompt('Enter new start date :');
+
+
+                // Send an AJAX request to update the employee record
+                $.ajax({
+                    url: 'http://localhost:3000/employee/' + employeeId,
+                    type: 'PUT',
+                    data: {
+                        name: newName,
+                        gender: newGender,
+                        department: newDepartment,
+                        salary: newSalary,
+                        startDate: newStartDate
+                    },
+                    success: function (data) {
+                        // Update the table row with the new data
+                        currentRow.find('td:nth-child(1)').html(
+                            $('<img>').attr('src', data.profileUrl),
+                            $('<span>').text(data.name)
+                        );
+                        currentRow.find('td:nth-child(2)').text(data.gender);
+                        currentRow.find('td:nth-child(3)').text(data.department);
+                        currentRow.find('td:nth-child(4)').text(data.salary);
+                        currentRow.find('td:nth-child(5)').text(data.startDate);
+                    },
+                    error: function (xhr, textStatus, errorThrown) {
+                        console.log('Error updating employee: ' + errorThrown);
+                    }
+                });
             }));
+
 
             actions.append($('<img>').attr('src', '../Assets/icons/delete-black-18dp.svg').attr('data-id', employee.id).click(function () {
                 var employeeId = $(this).attr('data-id');
@@ -43,6 +87,9 @@ function deleteEmployee(employeeId) {
         }
     });
 }
+
+
+
 
 
 
--- EmployeePayroll/Scripts/EmployeeData.js
+++ EmployeePayroll/Scripts/EmployeeData.js
@@ -1,49 +1,49 @@
-function SubmitForm(){
-    var name = $('#name').val();
-    console.log(name);
+function SubmitForm() {
+  var name = $('#name').val();
+  console.log(name);
 
-    var selectedImage = $('input[name="Profile"]:checked').val();
-    console.log(selectedImage);
+  var selectedImage = $('input[name="Profile"]:checked').val();
+  console.log(selectedImage);
 
-    var selectedGender = $('input[name="gender"]:checked').val();
-    console.log(selectedGender);
+  var selectedGender = $('input[name="gender"]:checked').val();
+  console.log(selectedGender);
 
-     var selectedDepartment = $('input[name="dept"]:checked').val();
-    
-    console.log(selectedDepartment);
+  var selectedDepartment = $('input[name="dept"]:checked').val();
 
-    var salary = $('#salary').val();
-    console.log(salary);
+  console.log(selectedDepartment);
 
-    var day = $('#Day').val();
-    var month = $('#Month').val();
-    var year = $('#Year').val();
-    console.log("Start Date = " + day + month + year);
+  var salary = $('#salary').val();
+  console.log(salary);
 
-    var note = $('#notes').val();
-    console.log(note);
+  var day = $('#Day').val();
+  var month = $('#Month').val();
+  var year = $('#Year').val();
+  console.log("Start Date = " + day + month + year);
 
-    let reqPayLoad = {
-        "name": name,
-        "profileUrl": selectedImage,
-        "gender": selectedGender,
-        "departMent": selectedDepartment,
-        "salary": salary,
-        "startDate": day + ' ' + month  + ' ' + year,
-        "notes": note
-      }
-      console.log(reqPayLoad);
+  var note = $('#notes').val();
+  console.log(note);
 
-      $.ajax({
-        url: 'http://localhost:3000/employee',
-        type: 'POST',
-        dataType: 'json',
-        data: reqPayLoad,
-        success: function (data, textStatus, xhr) {
-            console.log(data);
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log('Error in Operation');
-        }
+  let reqPayLoad = {
+    "name": name,
+    "profileUrl": selectedImage,
+    "gender": selectedGender,
+    "departMent": selectedDepartment,
+    "salary": salary,
+    "startDate": day + ' ' + month + ' ' + year,
+    "notes": note
+  }
+  console.log(reqPayLoad);
+
+  $.ajax({
+    url: 'http://localhost:3000/employee',
+    type: 'POST',
+    dataType: 'json',
+    data: reqPayLoad,
+    success: function (data, textStatus, xhr) {
+      console.log(data);
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log('Error in Operation');
+    }
   })
 }
